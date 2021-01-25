ccb8b68462206e283e0ea49afc115c62eff8570c
Alexey Bochkraev <a@bochkarev.io>
2021-01-25 18:47:48 +0100

    edited ts-literature (as presented)

---
+++ content-org/talks/auto/ts-literature.el
@@ -0,0 +1,52 @@
+(TeX-add-style-hook
+ "ts-literature"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-environments-local "minted")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "minted")
+   (LaTeX-add-labels
+    "sec:org6fe4560"
+    "sec:orgee9cdbd"
+    "sec:orgf648685"
+    "sec:org2c55bbd"
+    "sec:org959031f"
+    "sec:orga1d3abc"
+    "sec:org7ea76fd"
+    "sec:orgdcf75ac"
+    "sec:orgb0d9357"
+    "sec:org298d73d"
+    "sec:org4c1b87b"
+    "sec:orgccd3722"
+    "sec:orge695f75"
+    "sec:orge263674"
+    "sec:org94de392"))
+ :latex)
+
